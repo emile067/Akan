b6b1cf49756484d6e28da08cf742dc306d7ce420
emile kamana <emileizere@gmail.com>
2020-09-27 23:03:22 +0200

    add akan names and week days

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,14 @@ function submit() {
   var g= document.querySelector("input[name='gender']:checked").value;
   var day = new Date(document.getElementById("bday").value);
   var d = day.getDay();
+  var maleNames=['Kwasi',"Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 }
